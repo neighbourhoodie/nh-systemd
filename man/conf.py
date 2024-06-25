12274e3410dc110683a514ff6f1adff400fbae8d
Julia Krüger <juliakbln@gmail.com>
2024-06-25 19:40:00 +0200

    initial sphinx

--- man/conf.py
+++ man/conf.py
@@ -1,3 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'systemd'
+copyright = '2024, systemd'
+author = 'systemd'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
 global_substitutions = {
   'v183': 'Added in version 183',
   'v184': 'Added in version 184',
